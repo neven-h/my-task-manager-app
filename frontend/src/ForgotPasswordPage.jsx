2c81687375744de6c275a92609c67e0404348aed
Noa Even <noa.even0@gmail.com>
2026-01-10 17:19:27 +0200

    updated dependencies to include "react-router-dom": "^6.14.2" given the frontend check failure

---
+++ frontend/src/ForgotPasswordPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ForgotPasswordPage = () => {
+  return (
+    <div style={{ minHeight: '100vh', display:  'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <h1>Forgot Password - Coming Soon</h1>
+    </div>
+  );
+};
+
+export default ForgotPasswordPage;
---
+++ frontend/src/ResetPasswordPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResetPasswordPage = () => {
+  return (
+    <div style={{ minHeight:  '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <h1>Reset Password - Coming Soon</h1>
+    </div>
+  );
+};
+
+export default ResetPasswordPage;
